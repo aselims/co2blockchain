f051b10f66c0aa606ff4e2613f0fcfdc687acc8a
edmundus <lukas.maxeiner@bci.tu-dortmund.de>
2018-02-23 20:13:37 +0100

    Create simpleToken.sol

---
+++ simpleToken.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.18;
+
+contract simpleToken {
+    
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+    
+    event LogBalance(uint256 balance);
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function simpleToken(uint256 initialSupply) public {
+        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
+    }
+    
+    function getBalance() public {
+        LogBalance(balanceOf[msg.sender]);
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
+        require(balanceOf[_to] + _value >= balanceOf[_to]); // Check for overflows
+        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
+    }
+}
+
